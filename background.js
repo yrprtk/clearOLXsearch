029061668dbc05dcf325f672d3c6d652febb9f43
yrprtk <yura.pritykovskiy@gmail.com>
2021-07-01 22:32:02 +0300

    run for a specific page

--- background.js
+++ background.js
@@ -1,3 +1,13 @@
+chrome.tabs.onActivated.addListener((info) => {
+    setTimeout(async () => {
+        const tab = await chrome.tabs.get(info.tabId);
+        tab.url.startsWith('https://www.olx.ua/list/')
+            ? chrome.action.enable(tab.tabId) 
+            : chrome.action.disable(tab.tabId);
+    }, 500);
+});
+// https://stackoverflow.com/questions/10396634/chrome-extension-run-for-a-specific-page
+// https://stackoverflow.com/questions/67806779/im-getting-an-error-tabs-cannot-be-edited-right-now-user-may-be-dragging-a-ta
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.status === 'complete') {
         chrome.tabs.sendMessage(tabId, {
